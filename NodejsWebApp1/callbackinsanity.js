84bf9b4213432eb6e4086bf3cd2ee3a1db2b7885
adarshmandyam <adarsh.sreenivasan@gmail.com>
2017-08-29 23:03:53 +0100

    changes

---
+++ NodejsWebApp1/callbackinsanity.js
@@ -0,0 +1,19 @@
+﻿var fs = require('fs');
+
+fs.readdir('.', function (err, files) {
+    if (err) {
+        console.log('Error finding file' + err);
+    }
+    else {
+        files.forEach(function (fileName, fileIndex) {
+            fs.readFile(fileName, function (err, buf) {
+                if (err) {
+                    console.log('error reading file: ' + err);
+                }
+                else {
+                    console.log(buf.toString());
+                }
+            });
+        });
+    }
+});
